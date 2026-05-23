15b6194ba5804fb61bb7845c415b931164513704
Nisreen Mostafa AbdelRazek <nisreenm107@gmail.com>
2026-05-24 01:57:19 +0300

    express

---
+++ math.js
@@ -0,0 +1,11 @@
+export function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Inputs must be numbers!'); //Error handling addition
+    }
+    return a + b;
+}
+
+export function subtract(a, b){
+    return a - b;
+}
+module.exports = {add, subtract};
